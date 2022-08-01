80c885d56d0e4b79e7a0f9f8941160deb79e6386
Noel Lim <noellimx@gmail.com>
2022-08-02 07:20:18 +0800

    -

--- leetcode/00876-MiddleoftheLinkedList/grace_1.py
+++ leetcode/00876-MiddleoftheLinkedList/grace_1.py
@@ -5,14 +5,5 @@ class Solution:
             append_val = head
             arr.append(append_val)
             head = head.next
-        middle_index = len(arr) // 2 
+        middle_index = len(arr) // 2
         return arr[middle_index]
-
-
-        # alternative solution
-        # first = second = head
-        # while second and second.next:
-        #     first = first.next
-        #     second = second.next.next
-        # return first
-            

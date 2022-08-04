a07690a847184c0483d50def0e93034f50057624
Cheoklate <gcheok88@gmail.com>
2022-08-04 23:33:45 +0800

    Cheok 3sum

---
+++ leetcode/00015_Three_Sum/cheok.py
@@ -0,0 +1,27 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        def search_pair(nums, target_sum, l, triplets):
+            r = len(nums) - 1
+            while (l < r):
+                current_sum = nums[l] + nums[r]
+                if current_sum == target_sum:
+                    triplets.append([-target_sum, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r+1]:
+                        r -=1
+                elif target_sum > current_sum:
+                    l += 1
+                else:
+                    r -= 1                
+        
+        nums.sort()
+        triplets = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            search_pair(nums, -nums[i], i+1, triplets)
+        return triplets
+
